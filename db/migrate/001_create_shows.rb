caca8a439ecddd9b8af316c44b071b9bb55705f0
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 23:11:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration[4.2]
     create_table :shows do |t|
       t.string :name
       t.string :network
-      t.datetime :day
+      t.string :day
       t.integer :rating
   end
 end
